17f8135d747c0e9e039842f77092c628ae2d6b18
matheus.cordeiro <matheus.cordeiro@senior.com.br>
2021-02-18 13:03:14 -0300

    Ensure LoginRouter returns a custom error if no email or password are provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,12 +1,32 @@
 class LoginRouter {
     route (httpRequest) {
-        if (!httpRequest || !httpRequest.body) return { statusCode: 500 }
+        if (!httpRequest || !httpRequest.body) return HttpResponse.serverError() 
         const { email, password } = httpRequest.body
-        if (!email || !password) { 
-            return {
-                statusCode: 400
-            }
+        if (!email) return HttpResponse.badRequest('email')
+        if (!password)  return HttpResponse.badRequest('password')
+    }
+}
+
+class HttpResponse {
+    static badRequest(paramName) {
+        return {
+            statusCode: 400,
+            body: new MissingParamError(paramName)
         }
+    }
+
+    static serverError() {
+        return {
+            statusCode: 500,
+            body: new MissingParamError(paramName)
+        }
+    }
+}
+
+class MissingParamError extends Error{
+    constructor(paramName) {
+        super(`Missing param: ${paramName}`)
+        this.name
     }
 }
 
@@ -20,6 +40,7 @@ describe('LoginRouter', () => {
         }
         const httpResponse = sut.route(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError('email'))
     })
 
     test('Should return 400 if no password is provided', () => {
