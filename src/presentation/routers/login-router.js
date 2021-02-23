fe16bb671a2afa6b8ac81e95be225439ca5549a1
matheus.cordeiro <matheus.cordeiro@senior.com.br>
2021-02-23 12:53:18 -0300

    Ensure LoginRouter return 401 when invalid credentials are provided

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -10,6 +10,7 @@ module.exports = class LoginRouter {
         const { email, password } = httpRequest.body
         if (!email) return HttpResponse.badRequest('email')
         if (!password)  return HttpResponse.badRequest('password')
-        this.authUseCase.auth(email, password)        
+        this.authUseCase.auth(email, password)
+        return { statusCode: 401 }        
     }
 }
