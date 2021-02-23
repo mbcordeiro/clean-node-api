925e5f8fa3ce282b5f469d0d6ae6a9d0489fb1ca
matheus.cordeiro <matheus.cordeiro@senior.com.br>
2021-02-23 12:29:02 -0300

    Move sut creatino to a factory helper method

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,9 +1,13 @@
 const LoginRouter = require('./login-router');
 const MissingParamError = require('../helpers/missing-param-error');
 
+const makeSut = () => {
+    return new LoginRouter()
+}
+
 describe('LoginRouter', () => {
     test('Should return 400 if no email is provided', () => {
-        const sut = new LoginRouter()
+        const sut = makeSut()
         const httpRequest = {
             body : {
                 password: 'any_password'
@@ -15,7 +19,7 @@ describe('LoginRouter', () => {
     })
 
     test('Should return 400 if no password is provided', () => {
-        const sut = new LoginRouter()
+        const sut = makeSut()
         const httpRequest = {
             body : {
                 email: 'any_email@email.com'
@@ -26,14 +30,8 @@ describe('LoginRouter', () => {
     })
 
     test('Should return 500 if no httpRequest is provided', () => {
-        const sut = new LoginRouter()
+        const sut = makeSut()
         const httpResponse = sut.route()
-        expect(httpResponse.statusCode).toBe(500)
-    })
-    
-    test('Should return 500 if httpRequest has no body', () => {
-        const sut = new LoginRouter()
-        const httpResponse = sut.route({})
         expect(httpResponse.statusCode).toBe(500)
     })
 })
