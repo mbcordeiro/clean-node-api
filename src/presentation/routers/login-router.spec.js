ec8195690177a1b812afc272bf745b2c7dc07430
matheus.cordeiro <matheus.cordeiro@senior.com.br>
2021-02-23 12:47:10 -0300

    Ensure LoginRouter calls AuthUseCase with correct params

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -2,12 +2,22 @@ const LoginRouter = require('./login-router');
 const MissingParamError = require('../helpers/missing-param-error');
 
 const makeSut = () => {
-    return new LoginRouter()
+    class AuthUseCaseSpy { 
+        auth(email) { 
+            this.email = email,
+            this.password = this.password
+        }
+    }
+    const authUseCaseSpy = new AuthUseCaseSpy()
+    const sut =  new LoginRouter(authUseCaseSpy)
+    return { 
+        sut, authUseCaseSpy 
+    }
 }
 
 describe('LoginRouter', () => {
     test('Should return 400 if no email is provided', () => {
-        const sut = makeSut()
+        const { sut } = makeSut()
         const httpRequest = {
             body : {
                 password: 'any_password'
@@ -19,7 +29,7 @@ describe('LoginRouter', () => {
     })
 
     test('Should return 400 if no password is provided', () => {
-        const sut = makeSut()
+        const { sut } = makeSut()
         const httpRequest = {
             body : {
                 email: 'any_email@email.com'
@@ -30,8 +40,21 @@ describe('LoginRouter', () => {
     })
 
     test('Should return 500 if no httpRequest is provided', () => {
-        const sut = makeSut()
+        const { sut } = makeSut()
         const httpResponse = sut.route()
         expect(httpResponse.statusCode).toBe(500)
+    })
+
+    test('Should call AuthUseCase with correct params', () => {
+        const { sut } = makeSut()
+        const httpRequest = {
+            body : {
+                email: 'any_email@email.com',
+                password: 'any_password'
+            }
+        }
+        sut.route(httpRequest)
+        expect(authUseCaseSpy.email).toBe(httpRequest.body.email)
+        expect(authUseCaseSpy.password).toBe(httpRequest.body.password)
     })
 })
