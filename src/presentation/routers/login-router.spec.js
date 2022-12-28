276389d04d57ba2c2c0b92a440248f527ce88698
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-12-27 23:43:35 -0300

    Move generic errors to utils layer

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,5 +1,6 @@
 const LoginRouter = require('./login-router')
-const { MissingParamError, InvalidParamError, ServerError, UnauthorizedError } = require('../errors')
+const { ServerError, UnauthorizedError } = require('../errors')
+const { MissingParamError, InvalidParamError } = require('../../utils/errors')
 
 const makeSut = () => {
   const authUseCaseSpy = makeAuthUseCase()
